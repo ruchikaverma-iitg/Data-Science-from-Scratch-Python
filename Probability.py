30283a0f6248c400405a8902f0fab88f9a155870
Ruchika Verma <verma.ece.ruchika@gmail.com>
2019-07-08 08:27:06 -0400

    Update Probability.py

--- Probability.py
+++ Probability.py
@@ -111,7 +111,8 @@ plt.legend()
 plt.title("Various normal pdfs")
 plt.show()
 
-#Inverse of a normal distribution
+# Inverse of a normal distribution
+# Returns the z for which P(Z<=z) = p
 def inverse_normal_cdf(p:float,mu:float=0,sigma:float = 1,tolerance:float=0.00001)-> float:
     if mu!=0 or sigma!=1:
         return mu+sigma*inverse_normal_cdf(p,tolerance=tolerance)
